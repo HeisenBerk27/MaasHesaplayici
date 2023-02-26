ab203c1505b90839381ade5d26c12b07f58e8c9d
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-26 20:15:47 +0300

    Add files via upload

---
+++ src/sinif/MainEmployee.java
@@ -0,0 +1,13 @@
+package sinif;
+
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee("Berk",31000.0,55,2003);
+		
+		
+		System.out.println(e1.toString());
+	}
+
+}
